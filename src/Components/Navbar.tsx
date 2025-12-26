fedaf0df4c375b76e9c2023eb84c8fb982591871
Dany-Foster <mampionontsoadiano@gmail.com>
2025-12-26 12:00:13 +0300

    configuration

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { navbarLinks } from "../data/Factice";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="logo" />
+        <ul>
+          {navbarLinks.map((link) => (
+            <li>
+              <a href={link.href}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex gap-3">
+          <button>
+            <img src="/search.svg" alt="search" />
+          </button>
+          <button>
+            <img src="/cart.svg" alt="cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/data/Factice.ts
@@ -0,0 +1,8 @@
+export const navbarLinks = [
+  { label: "Store", href: "/" },
+  { label: "Mac", href: "/Mac" },
+  { label: "Iphone", href: "/Iphone" },
+  { label: "Watch", href: "/Watch" },
+  { label: "Vision", href: "/Vision" },
+  { label: "AirPods", href: "/AirPods" },
+];
