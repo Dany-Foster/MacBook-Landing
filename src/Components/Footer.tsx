cd36ebac51f8880f5dcb54decd1047a10e8849ec
Dany-Foster <mampionontsoadiano@gmail.com>
2026-01-20 10:11:20 +0300

    Implementation Footer Section

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,5 +1,28 @@
+import { footerLinks } from "../data/Factice";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find an Apple Store or other retailer near you. Or
+          call 000800 040 1966. <img src="/logo.svg" alt="Apple logo" />
+        </p>
+      </div>
+
+      <hr />
+      <div className="links">
+        <p>Coyright @ 2024 Apple Inc. All rights reserved. </p>
+        <ul>
+          {footerLinks.map((link, index) => (
+            <li key={index}>
+              <a href={link.link}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
